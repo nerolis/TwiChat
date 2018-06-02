09dc8b954fd465f8b9363c586f072a8506f8974f
nerolis <yakumoft@gmail.com>
2018-06-03 00:00:54 +0300

    Add project files.

---
+++ WpfApp1/API.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WpfApp1
+{
+    class API
+    {   
+
+        public void SendMsgToChat(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void ConnectChannel(string channel)
+        {
+            Console.WriteLine(channel);
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        API Twitch = new API();
+
+        public MainWindow()
+
+        {
+            InitializeComponent();
+        }
+
+        private void JoinChannel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChatInput_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void SendMsgToChat_Click(object sender, RoutedEventArgs e)
+        {
+            if (ChatInput.Text == "")
+            {
+                MessageBox.Show("You must write something before send");
+            } else
+            {
+                Twitch.SendMsgToChat(ChatInput.Text);
+                MsgBox.Text = MsgBox.Text + "\n" + ChatInput.Text;
+                ChatInput.Text = "";
+            }   
+        }
+    }
+}
